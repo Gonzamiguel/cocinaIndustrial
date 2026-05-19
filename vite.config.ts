02c879cdd16549d9ec62b768d865e0dd865f7a84
Gonzalo Miguel <gonzalo.augustin.miguel@gmail.com>
2026-05-19 18:14:53 -0300

    arreglos pwa

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,svg,png,webp,woff2}'],
         navigateFallback: 'index.html',
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
     }),
   ],
